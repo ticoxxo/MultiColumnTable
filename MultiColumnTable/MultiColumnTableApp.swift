a2c01d42e56397fea741b8d4d3bb645cb2932c1f
Ticoxxo <ticoxxo@gmail.com>
2024-05-28 09:21:26 -0700

    Initial Commit

---
+++ MultiColumnTable/MultiColumnTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiColumnTableApp.swift
+//  MultiColumnTable
+//
+//  Created by Alberto Almeida on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiColumnTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
